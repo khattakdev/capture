f2790f120a52b188042c4d9a334359586e164184
Arsalan Khattak <me@khattak.dev>
2023-10-11 13:00:02 +0500

    ✨ feat: add basic image saving functionality with static text

--- src/extension.ts
+++ src/extension.ts
@@ -1,25 +1,56 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
+import * as fs from "fs";
+const puppeteer = require("puppeteer");
+// import * as puppeteer from "puppeteer";
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+  // Use the console to output diagnostic information (console.log) and errors (console.error)
+  // This line of code will only be executed once when your extension is activated
+  console.log('Congratulations, your extension "capture" is now active!');
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "capture" is now active!');
+  // The command has been defined in the package.json file
+  // Now provide the implementation of the command with registerCommand
+  // The commandId parameter must match the command field in package.json
+  let disposable = vscode.commands.registerCommand(
+    "capture.snapshot",
+    async () => {
+      // The code you place here will be executed every time your command is executed
+      // Display a message box to the user
+      const browser = await puppeteer.launch();
+      const page = await browser.newPage();
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('capture.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from capture!');
-	});
+      await page.setContent(
+        `<pre><code>const i = "Hello World!";</code></pre>`
+      );
+      const imageBuffer = await page.screenshot();
 
-	context.subscriptions.push(disposable);
+      const fileUri = await vscode.window.showSaveDialog({
+        filters: {
+          Images: ["png"],
+        },
+        saveLabel: "save Images",
+        title: "Save Code Snippet Image",
+      });
+
+      if (fileUri) {
+        fs.writeFile(fileUri.fsPath, imageBuffer, (err) => {
+          if (err) {
+            vscode.window.showErrorMessage("Failed to save imaged!");
+          } else {
+            vscode.window.showInformationMessage("Image saved!");
+          }
+        });
+      }
+      await browser.close();
+      vscode.window.showInformationMessage("Snapshot taken!");
+    }
+  );
+
+  context.subscriptions.push(disposable);
 }
 
 // This method is called when your extension is deactivated
---
+++ src/template.ts
@@ -0,0 +1,67 @@
+export const template = `
+<html lang="en">
+    <head>
+      <style>
+        body {
+          background: #282c33;
+          padding: 0px;
+          margin: 0px;
+        }
+  
+        .header {
+          background: #343840;
+          height: 50px;
+          width: full;
+          position: relative;
+        }
+  
+        .dots {
+          position: absolute;
+          top: 50%;
+          left: 4%;
+          transform: translateY(-50%);
+          display: flex;
+        }
+  
+        .dot {
+          height: 15px;
+          width: 15px;
+          border-radius: 50%;
+          margin: 0px 5px;
+        }
+  
+        .dot-1 {
+          left: 2%;
+          background: #e74c3c;
+        }
+  
+        .dot-2 {
+          background: #f1c40f;
+        }
+  
+        .dot-3 {
+          background: #2ecc71;
+        }
+  
+        .body {
+          color: white;
+          padding: 20px;
+          font-size: 28px;
+        }
+      </style>
+    </head>
+    <body>
+      <div class="header">
+        <div class="dots">
+          <div class="dot dot-1"></div>
+          <div class="dot dot-2"></div>
+          <div class="dot dot-3"></div>
+        </div>
+      </div>
+      <div class="body">
+      const img = "YAY! a new image!";</div>
+    </body>
+  </html>
+  `;
+
+// export default template;
