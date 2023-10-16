ada13867592542928f3e794d426584178475b6cb
Hammad Saeedi <faoqzuhair@gmail.com>
2023-10-16 19:54:50 +0500

    🚀 Added image opening functionality
    Modified the extension code to open images with a clickable link. Now, when you save an image, you can click on the "Open Image" link in the information message to open the image using the default system image viewer.

--- src/extension.ts
+++ src/extension.ts
@@ -15,56 +15,59 @@ export function activate(context: vscode.ExtensionContext) {
   // The command has been defined in the package.json file
   // Now provide the implementation of the command with registerCommand
   // The commandId parameter must match the command field in package.json
-  let disposable = vscode.commands.registerCommand(
-    "capture.snapshot",
-    async () => {
-      const editor = vscode.window.activeTextEditor;
-      if (editor) {
-        const document = editor.document;
-        const selection = editor.selection;
-        const text = document.getText(selection);
+  let disposable = vscode.commands.registerCommand("capture.snapshot", async () => {
+    const editor = vscode.window.activeTextEditor;
+    if (editor) {
+      const document = editor.document;
+      const selection = editor.selection;
+      const text = document.getText(selection);
 
-        const browser = await puppeteer.launch();
-        const page = await browser.newPage();
+      const browser = await puppeteer.launch();
+      const page = await browser.newPage();
 
-        const content = await generateTemplate(text);
-        await page.setContent(content);
-        const contentHeight = content.split("\n").length;
-        const totalHeight = contentHeight * 3;
-        await page.setViewport({
-          width: 800,
-          height: totalHeight + 50,
+      const content = await generateTemplate(text);
+      await page.setContent(content);
+      const contentHeight = content.split("\n").length;
+      const totalHeight = contentHeight * 3;
+      await page.setViewport({
+        width: 800,
+        height: totalHeight + 50,
+      });
+      const imageBuffer = await page.screenshot({
+        fullPage: true,
+      });
+
+      // Show a save dialog to choose the image file location
+      const fileUri = await vscode.window.showSaveDialog({
+        filters: {
+          images: ["png"],
+        },
+        saveLabel: "Save Image",
+        title: "Save Code Snippet Image",
+      });
+
+      if (fileUri) {
+        fs.writeFile(fileUri.fsPath, imageBuffer, (err) => {
+          if (err) {
+            vscode.window.showErrorMessage("Failed to save image!");
+          } else {
+            // Create a vscode.Uri for the saved image
+            const imageUri = vscode.Uri.file(fileUri.fsPath);
+
+            // Show an information message with a clickable link to open the image
+            vscode.window.showInformationMessage("Image saved. Click to open.", "Open Image").then((choice) => {
+              if (choice === "Open Image") {
+                vscode.env.openExternal(imageUri);
+              }
+            });
+          }
         });
-        const imageBuffer = await page.screenshot({
-          fullPage: true,
-        });
-
-        const fileUri = await vscode.window.showSaveDialog({
-          filters: {
-            // eslint-disable-next-line @typescript-eslint/naming-convention
-            Images: ["png"],
-          },
-          saveLabel: "Save Image",
-          title: "Save Code Snippet Image",
-        });
-
-        if (fileUri) {
-          fs.writeFile(fileUri.fsPath, imageBuffer, (err) => {
-            if (err) {
-              vscode.window.showErrorMessage("Failed to save imaged!");
-            } else {
-              vscode.window.showInformationMessage("Image saved!");
-            }
-          });
-        }
-        await browser.close();
-        // vscode.window.showInformationMessage("Snapshot taken!");
       }
-      // The code you place here will be executed every time your command is executed
-      // Display a message box to the user
+      await browser.close();
     }
-  );
+  });
 
+  // Add the command to the context subscriptions
   context.subscriptions.push(disposable);
 }
 
