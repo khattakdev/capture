0758284dac621e1d69059198bc5fda4440393543
Sujal <115341188+coding-cosmos@users.noreply.github.com>
2023-10-17 14:54:54 +0530

    feat: Show error on no selection (#17)
    * feat: Show error on no selection
    
    * Update Error Message

--- src/extension.ts
+++ src/extension.ts
@@ -24,6 +24,11 @@ export function activate(context: vscode.ExtensionContext) {
         const selection = editor.selection;
         const text = document.getText(selection);
 
+        if (text.trim() === "") {
+          vscode.window.showErrorMessage("You have not highlighted anything.");
+          return;
+        } 
+        
         const browser = await puppeteer.launch();
         const page = await browser.newPage();
 
